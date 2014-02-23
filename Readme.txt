fbd6511bdc9107b5eb0ec6f9749794f8c162c5a0
alphez <kristianstorvoll@gmail.com>
2014-02-23 22:24:33 +0100

    Opprydding

--- Readme.txt
+++
@@ -1,1 +0,0 @@
-Scanne strekkoder
--- intelligenceTest/gen/com/example/intelligencetest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.intelligencetest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}

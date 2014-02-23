ffbecf1aa73c9ba7bbee838de40ca693adc3ba76
pumalove <leedslyngstad@hotmail.com>
2014-02-23 22:23:32 +0100

    Oppdatert gridview

--- intelligenceTest/gen/com/example/intelligencetest/R.java
+++ intelligenceTest/gen/com/example/intelligencetest/R.java
@@ -37,29 +37,32 @@ public final class R {
         public static final int scrollbar_handle_holo_light=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int button1=0x7f080007;
-        public static final int dividerText=0x7f080006;
+        public static final int action_settings=0x7f08000e;
+        public static final int button1=0x7f080009;
+        public static final int dividerText=0x7f080008;
         public static final int gridview=0x7f080003;
         public static final int listView1=0x7f080002;
-        public static final int list_image=0x7f080009;
+        public static final int list_image=0x7f08000b;
         public static final int pager=0x7f080000;
-        public static final int personAge=0x7f08000b;
-        public static final int personName=0x7f08000a;
-        public static final int person_thumbnail=0x7f080008;
-        public static final int qrscan_btn=0x7f080005;
-        public static final int scan_btn=0x7f080004;
+        public static final int personAge=0x7f08000d;
+        public static final int personName=0x7f08000c;
+        public static final int person_thumbnail=0x7f08000a;
+        public static final int picture=0x7f080004;
+        public static final int qrscan_btn=0x7f080007;
+        public static final int scan_btn=0x7f080006;
         public static final int section_label=0x7f080001;
+        public static final int text=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main_dummy=0x7f030001;
         public static final int fragment_main_persons=0x7f030002;
         public static final int gridview=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int person_list_section_divider=0x7f030005;
-        public static final int scan=0x7f030006;
-        public static final int simple_list_item_1=0x7f030007;
+        public static final int gridview_item=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int person_list_section_divider=0x7f030006;
+        public static final int scan=0x7f030007;
+        public static final int simple_list_item_1=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- intelligenceTest/src/com/example/scanner/GridMenu.java
+++ intelligenceTest/src/com/example/scanner/GridMenu.java
@@ -1,38 +1,104 @@
 package com.example.scanner;
 
-import com.example.intelligencetest.R;
+import java.util.ArrayList;
+import java.util.List;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.intelligencetest.R;
 
+public class GridMenu extends Fragment
+{
 
-public class GridMenu extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+    	    ViewGroup container, Bundle savedInstanceState)
+    	    {
+    	        View view = inflater.inflate(
+    	            R.layout.gridview, container, false);
+    	        
+        GridView gridView = (GridView)view.findViewById(R.id.gridview);
+        gridView.setAdapter(new MyAdapter(getActivity()));
+		return gridView;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		View view = inflater.inflate(R.layout.gridview, container, false);
-		  GridView gridview = (GridView) view.findViewById(R.id.gridview);
-		    gridview.setAdapter(new ImageAdapter(getActivity()));
+    private class MyAdapter extends BaseAdapter
+    {
+        private List<Item> items = new ArrayList<Item>();
+        private LayoutInflater inflater;
 
-		    gridview.setOnItemClickListener(new OnItemClickListener() {
-		        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-		            Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
-		        }
-		
-		
-	});
-			return gridview;
-	
-	
-}
+        public MyAdapter(Context context)
+        {
+            inflater = LayoutInflater.from(context);
+
+            items.add(new Item("Image 1", R.drawable.sample_0));
+            items.add(new Item("Image 2", R.drawable.sample_1));
+            items.add(new Item("Image 3", R.drawable.sample_2));
+            items.add(new Item("Image 4", R.drawable.sample_3));
+            items.add(new Item("Image 5", R.drawable.sample_4));
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i)
+        {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i)
+        {
+            return items.get(i).drawableId;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup)
+        {
+            View v = view;
+            ImageView picture;
+            TextView name;
+
+            if(v == null)
+            {
+               v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
+               v.setTag(R.id.picture, v.findViewById(R.id.picture));
+               v.setTag(R.id.text, v.findViewById(R.id.text));
+            }
+
+            picture = (ImageView)v.getTag(R.id.picture);
+            name = (TextView)v.getTag(R.id.text);
+
+            Item item = (Item)getItem(i);
+
+            picture.setImageResource(item.drawableId);
+            name.setText(item.name);
+
+            return v;
+        }
+
+        private class Item
+        {
+            final String name;
+            final int drawableId;
+
+            Item(String name, int drawableId)
+            {
+                this.name = name;
+                this.drawableId = drawableId;
+            }
+        }
+    }
 }
